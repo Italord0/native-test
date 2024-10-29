788d06c9fb2f53969985ffcd2929628c41539d6c
italo <italo.melo@g.globo>
2024-10-28 22:08:14 -0300

    removed publishin maven

--- multicontent/build.gradle.kts
+++ multicontent/build.gradle.kts
@@ -1,23 +1,10 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
-    id("maven-publish")
 }
 
 val versionName = findProperty("VERSION_NAME") as String
 version = versionName
-
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            from(components["release"])
-
-            groupId = "com.github.italord0"
-            artifactId = "multicontent"
-            version = version
-        }
-    }
-}
 
 android {
     namespace = "com.example.multicontent"
@@ -52,7 +39,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
